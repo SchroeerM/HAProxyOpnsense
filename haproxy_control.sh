8b894c9ea9e396818b11f19253a53b6b5f187753
SchroeerM <48346202+SchroeerM@users.noreply.github.com>
2024-08-28 11:35:12 +0200

    Create haproxy_control.sh
    Initial

---
+++ haproxy_control.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Definiere die CARP-Interface-Variable (ändere sie entsprechend deiner Umgebung)
+CARP_INTERFACE="vhid0"
+
+# Status des CARP-Interfaces abfragen
+CARP_STATUS=$(ifconfig $CARP_INTERFACE | grep 'carp: MASTER' | wc -l)
+
+# HAProxy steuern
+if [ "$CARP_STATUS" -eq 1 ]; then
+    # Wenn CARP Master, starte HAProxy
+    if ! pgrep -x "haproxy" > /dev/null
+    then
+        echo "Starting HAProxy as this is the CARP Master."
+        service haproxy start
+    else
+        echo "HAProxy is already running."
+    fi
+else
+    # Wenn nicht CARP Master, stoppe HAProxy
+    if pgrep -x "haproxy" > /dev/null
+    then
+        echo "Stopping HAProxy as this is not the CARP Master."
+        service haproxy stop
+    else
+        echo "HAProxy is already stopped."
+    fi
+fi
